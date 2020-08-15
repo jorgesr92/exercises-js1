df927423be73991a0fd0b6608d79594633d29d2a
jorgesr92 <jorgesr92@github.com>
2020-08-15 13:59:55 +0200

    add exercise-H

--- week-1/InClass/exercise-H.js
+++ week-1/InClass/exercise-H.js
@@ -1,1 +1,10 @@
-console.log(4%3)
+function numOperators(numA, numB){
+    //esta función nos da la multiplicación y la división entre los dos numeros dados
+    let mult = (numA * numB).toFixed(2);
+    let div = (numA / numB).toFixed(2);
+    let message = `La multiplicación de `.concat(numA, " y ", numB, " es: ", mult);
+    let message2 = `La división de `.concat(numA, " y ", numB, " es: ", div);
+    return `${ message } \n${ message2 }`;
+}
+
+console.log(numOperators(20,4.5))
