7341a6b753c498c133d532e3cbe015fbc22d70a5
jorgesr92 <jorgesr92@github.com>
2020-08-15 13:08:48 +0200

    Extra exercise

--- week-1/Homework/L-functions-nested/exercise2.js
+++ week-1/Homework/L-functions-nested/exercise2.js
@@ -2,7 +2,6 @@
 let mentors = ["Daniel", "Irina", "Mimi", "Rob", "Yohannes"];
 
 function greeting(name){
-    // let newName = name.toUpperCase();
     return `HELLO ${ name.toUpperCase() }`;
 }
 for (x in mentors){
