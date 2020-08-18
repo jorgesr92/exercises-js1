4703c0d5f115831c6865c5ffc89889b242ba7230
jorgesr92 <jorgesr92@github.com>
2020-08-18 19:49:02 +0200

    add exercise C

--- week-2/InClass/exercise-C.js
+++ week-2/InClass/exercise-C.js
@@ -1,7 +1,7 @@
 function numberChecker(num) {
   if (num > 20) {
     return `${num} is greater than 20`;
-  } else if (num === 20) {
+  } else if (num == 20) {
     return `${num} is equal to 20`;
   } else if (num < 20) {
     return `${num} is less than 20`;
@@ -9,3 +9,8 @@ function numberChecker(num) {
     return `${num} isn't even a number :(`;
   }
 }
+
+console.log(numberChecker(4));
+console.log(numberChecker(34));
+console.log(numberChecker(20));
+console.log(numberChecker("No soy un Número"));
