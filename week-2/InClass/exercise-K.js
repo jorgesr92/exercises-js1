e614f2f562c2838b9f3b6bc4f4fd9bc603f5ada4
jorgesr92 <jorgesr92@github.com>
2020-08-22 14:33:08 +0200

    add exercise K

--- week-2/InClass/exercise-K.js
+++ week-2/InClass/exercise-K.js
@@ -2,7 +2,7 @@ let arrStudent = ["Juan", "Daniel", "Carlos", "Jorge", "Antonio"];
 
 function getNameStudent(arr){
     for (x in arr){
-        console.log(`El nombre del estudiante es: ${ arr[x] }`);
+        console.log(`El nombre del estudiante ${ parseInt(x) + 1 } es: ${ arr[x] }`);
     }
 }
 
