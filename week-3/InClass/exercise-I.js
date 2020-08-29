c997ed78808c3bad61decbcfb41d46f533933da1
jorgesr92 <jorgesr92@github.com>
2020-08-29 14:32:29 +0200

    add exercise I

---
+++ week-3/InClass/exercise-I.js
@@ -0,0 +1,19 @@
+const arr = [
+    100,
+    "iSMael",
+    55,
+    45,
+    "sANyiA",
+    66,
+    "JaMEs",
+    "eLAmIn",
+    23,
+    "IsMael",
+    67,
+    19,
+    "ElaMIN",
+  ];
+
+
+
+  const newArr = arr.filter(el => typeof el === "string").map(element => element.toUpperCase().concat("!")).forEach(element => console.log(element));
